4e4729410baa904876181491f27d72c4c134ebf9
Mathieu Maes <mathieu@MBP-van-Mathieu.fritz.box>
2015-01-03 21:11:35 +0100

    Last minute changes for release 0.1

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,11 +3,16 @@
 namespace ConcertoCms\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ConcertoCmsAdminBundle:Default:index.html.twig');
+        $router = $this->get('router');
+        $uri = $router->generate('concerto_cms_admin_pages');
+        return new RedirectResponse($uri, 301);
+
+        //return $this->render('ConcertoCmsAdminBundle:Default:index.html.twig');
     }
 }
